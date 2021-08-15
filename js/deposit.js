fc00f1349608a3cb766cd9a45ebd84bf73f17830
FarhanNadim12 <farhannadim1212@gmail.com>
2021-08-15 16:04:18 +0600

    hdhewf

--- js/deposit.js
+++ js/deposit.js
@@ -5,19 +5,19 @@ document.getElementById("deposit-btn").addEventListener("click", function () {
     if (depositFieldValue.value >= 0) {
         let depositField = document.getElementById("deposit-field");
         let depositAmount = depositField.value;
-        let depositAmountNum = parseInt(depositAmount);
+        let depositAmountNum = parseFloat(depositAmount);
         // console.log(typeof depositAmountNum);
         depositField.value = '';
         //adding money in deposit
         let displayDepositValue = document.getElementById("displayDeposit").innerText;
-        const displayDepositValueNum = parseInt(displayDepositValue);
+        const displayDepositValueNum = parseFloat(displayDepositValue);
         // console.log(typeof displayDepositValueNum)
         const totalDeposit = displayDepositValueNum + depositAmountNum;
 
         document.getElementById("displayDeposit").innerText = totalDeposit;
         // adding money in balance
         let displayBalanceValue = document.getElementById("displayBalance").innerText;
-        const displayBalanceValueNum = parseInt(displayBalanceValue);
+        const displayBalanceValueNum = parseFloat(displayBalanceValue);
         const totalBalance = displayBalanceValueNum + depositAmountNum;
         document.getElementById("displayBalance").innerText = totalBalance;
     } else {
@@ -25,6 +25,7 @@ document.getElementById("deposit-btn").addEventListener("click", function () {
     }
 
 })
+
 
 
 
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,25 +1,28 @@
 const withdrawFieldValue = document.getElementById("withdraw-field");
 
+
 document.getElementById("withdraw-btn").addEventListener("click", function () {
     if (withdrawFieldValue.value >= 0) {
         let withdrawField = document.getElementById("withdraw-field");
         let withdrawAmount = withdrawField.value;
-        let withdrawAmountNum = parseInt(withdrawAmount);
+        let withdrawAmountNum = parseFloat(withdrawAmount);
         // console.log(typeof depositAmountNum);
         withdrawField.value = '';
         //adding money in withdraw
         let displayWithdrawValue = document.getElementById("displayWithdraw").innerText;
-        const displayWithdrawValueNum = parseInt(displayWithdrawValue);
+        const displayWithdrawValueNum = parseFloat(displayWithdrawValue);
         // console.log(typeof displayDepositValueNum)
         const totalWithdraw = displayWithdrawValueNum + withdrawAmountNum;
 
         document.getElementById("displayWithdraw").innerText = totalWithdraw;
         // adding money in balance
         let displayBalanceValue = document.getElementById("displayBalance").innerText;
-        const displayBalanceValueNum = parseInt(displayBalanceValue);
+        const displayBalanceValueNum = parseFloat(displayBalanceValue);
         const totalBalance = displayBalanceValueNum - withdrawAmountNum;
         document.getElementById("displayBalance").innerText = totalBalance;
-    } else {
+    }
+
+    else {
         alert("please withdraw positive value")
     }
 
